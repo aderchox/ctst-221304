30800a82625d46f07ab75dc954058879cfb33eda
aderchox <aderchox@gmail.com>
2022-06-05 19:51:57 +0430

    Refactors code for better readability (Thanks to Marcin Wanago).
    - Makes functions pure(Thanks to https://wanago.io/2019/09/30/improving-code-pure-functions/).
    - Uses better names for variables (non-ninja names D:).

--- dynamic.js
+++ dynamic.js
@@ -1,59 +1,45 @@
-const enLang = {
+const enLanguage = {
   name: "en",
   dir: "ltr",
 };
 
-const faLang = {
+const faLanguage = {
   name: "fa",
   dir: "rtl",
 };
 
-function getLang() {
-  const currentLang = JSON.parse(localStorage.getItem("lang"));
-  if (!currentLang) {
-    // Default lang (on first run) will be enLang (English)
-    localStorage.setItem("lang", JSON.stringify(enLang));
-    return enLang;
-  } else return currentLang;
+function getLanguage() {
+  const currentLanguage = JSON.parse(localStorage.getItem("language"));
+  if (!currentLanguage) {
+    // Default language (on first run) will be enLanguage (English)
+    localStorage.setItem("language", JSON.stringify(enLanguage));
+    return enLanguage;
+  } else {
+    return currentLanguage;
+  }
 }
 
-document.querySelector("nav > button").addEventListener("click", () => {
-  const currentLang = getLang();
-  if (currentLang?.name === "en") {
-    localStorage.setItem("lang", JSON.stringify(faLang));
-  } else if (currentLang?.name === "fa") {
-    localStorage.setItem("lang", JSON.stringify(enLang));
-  } else {
-    console.error("invalid langauge requested");
-  }
+async function initializeData() {
+  let gamesContents = null;
 
-  activate();
-  return;
-});
+  const gamesResponse = await fetch("./data/games.json");
+  const gamesData = await gamesResponse.json();
+  const game = gamesData[getActiveGameButtonIndex()];
 
-let gameElem = null;
-let gamesContents = null;
-
-async function initData() {
-  let game = null,
-    games;
-  games = await fetch("./data/games.json");
-  games = await games.json();
-  game = games[getActiveGameBtnIdx()];
-
-  await fetch(`./languages/${getLang()?.name}.json`).then(
-    async (resp) => (gamesContents = await resp.json())
+  await fetch(`./languages/${getLanguage()?.name}.json`).then(
+    async (response) => (gamesContents = await response.json())
   );
 
-  gameElem = createGameElem(game);
+  const gameElement = createGameElement(game, gamesContents);
+  return gameElement;
 }
 
-function createGameElem(game) {
+function createGameElement(game, gamesContents) {
   const main = document.createElement("main");
   main.innerHTML = `<div class="info">
   <span>${game.name}</span>
-  <h2>${gamesContents?.GAMES[getActiveGameBtnIdx()].HEADING}</h2>
-  <p>${gamesContents?.GAMES[getActiveGameBtnIdx()].DESCRIPTION}</p>
+  <h2>${gamesContents?.GAMES[getActiveGameButtonIndex()].HEADING}</h2>
+  <p>${gamesContents?.GAMES[getActiveGameButtonIndex()].DESCRIPTION}</p>
   <button>${gamesContents?.GENERAL?.CALL_TO_ACTION}</button>
 </div>
 
@@ -65,36 +51,60 @@ function createGameElem(game) {
 }
 
 function initEvents() {
+  initializeChangeLanguageEvent();
+  initializeGameButtonsEvents();
+}
+
+function initializeChangeLanguageEvent() {
+  document.querySelector("nav > button").addEventListener("click", () => {
+    const currentLanguage = getLanguage();
+    if (currentLanguage?.name === "en") {
+      localStorage.setItem("language", JSON.stringify(faLanguage));
+    } else if (currentLanguage?.name === "fa") {
+      localStorage.setItem("language", JSON.stringify(enLanguage));
+    } else {
+      console.error("invalid langauge requested");
+    }
+
+    activate();
+    return;
+  });
+}
+
+function initializeGameButtonsEvents() {
   const gameButtons = document.querySelectorAll("nav ul li button");
 
-  for (const [idx, gameBtn] of gameButtons.entries()) {
-    gameBtn.addEventListener("click", () => {
-      gameButtons.forEach((gameBtn, i) => {
-        if (i !== idx) {
-          gameBtn.classList.remove("active");
-        } else {
-          gameBtn.classList.add("active");
-        }
-      });
-
+  for (const [index, gameButton] of gameButtons.entries()) {
+    gameButton.addEventListener("click", () => {
+      moveActiveClassTo(gameButtons, index);
       activate();
+    });
+  }
+
+  function moveActiveClassTo(gameButtons, indexToActive) {
+    gameButtons.forEach((gameButton, index) => {
+      if (index !== indexToActive) {
+        gameButton.classList.remove("active");
+      } else {
+        gameButton.classList.add("active");
+      }
     });
   }
 }
 
 async function activate() {
-  await initData();
+  const gameElement = await initializeData();
   const htmlElem = document.documentElement;
 
-  const currentLang = getLang();
-  htmlElem.lang = currentLang?.name;
-  htmlElem.dir = currentLang?.dir;
+  const currentLanguage = getLanguage();
+  htmlElem.lang = currentLanguage?.name;
+  htmlElem.dir = currentLanguage?.dir;
 
   const main = document.querySelector("main");
-  main.innerHTML = gameElem.innerHTML;
+  main.innerHTML = gameElement.innerHTML;
 }
 
-function getActiveGameBtnIdx() {
+function getActiveGameButtonIndex() {
   return (
     parseInt(document.querySelector("nav ul li button.active").innerHTML) - 1
   );
